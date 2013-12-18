c4971ad8d47e080600da845c85a5acafb88942b1
forever-ram <ramsharma.forever@gmail.com>
2013-12-18 23:29:00 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NVRaw::Application.config.secret_token = 'c6613060979c903b3000c6f771ffd3171e405d16edf33fe8a155f8d2733196cba034d49ee3aa4d469e86bef9256cbc525c4bb4d31ef03e9661762891f9f4b1e5'
