536b1eab28f63f622f98c50542f1cf3d8e1e3314
forever-ram <ramsharma.forever@gmail.com>
2013-12-20 14:07:41 +0530

    add pricing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 NVRaw::Application.routes.draw do
 
   get '/about-us' => "welcome#about_us", as: :about_us
+  get '/pricing' => "welcome#pricing", as: :pricing
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
