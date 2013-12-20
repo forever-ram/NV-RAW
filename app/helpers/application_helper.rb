dd6f6cf388ffbe22795998fe0a26b4dde32d32cf
forever-ram <ramsharma.forever@gmail.com>
2013-12-20 16:10:08 +0530

    add services page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def apply_active_class_on_drop_down(controller, action)
-    actions = ['features']
+    actions = ['features', 'services']
     return controller == 'welcome' && actions.include?(action) ? 'active' : ''
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ NVRaw::Application.routes.draw do
   get '/contact' => "welcome#contact", as: :contact
   get '/blog' => "welcome#blog", as: :blog
   get '/features' => "welcome#features", as: :features
+  get '/services' => "welcome#services", as: :services
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
